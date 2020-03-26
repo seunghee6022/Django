20c8ffe8ef0bc25823f17afc2e575f513a56c484
정승희 <seunghee6022@gmail.com>
2020-03-26 15:11:07 +0900

    first commit

---
+++ 00_django_intro/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def lotto(request):
+    import random
+    pick = random.sample(range(1,46),6)
+    context= { 'pick' : pick}
+    return render(request, 'lotto.html', context)
