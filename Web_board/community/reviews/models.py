6183a422fc029689fa136a9c04f5b96edc26eed3
Seunghee Jeong <seunghee6022@gmail.com>
2020-04-17 13:46:35 +0900

    edit models.py| C(Create) finction - new.html

--- Web_board/community/reviews/models.py
+++ Web_board/community/reviews/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Review(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    rank = models.IntegerField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
--- Web_board/community/reviews/views.py
+++ Web_board/community/reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Review
 
 # Create your views here.
@@ -8,3 +8,18 @@ def index(request):
         'reviews' : reviews
     }
     return render(request,'reviews/index.html',context)
+
+def new(request):
+    return render(request, 'reviews/new.html')
+
+
+def create(request):
+    review = Review()
+    review.title = request.GET.get('title')
+    review.content = request.GET.get('content')
+    review.rank = request.GET.get('rank')
+    review.save()
+    return redirect('reviews:index')
+
+
+        
