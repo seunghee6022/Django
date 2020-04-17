f939c05d6e1356b3da6e77722823947f6d959115
Seunghee Jeong <seunghee6022@gmail.com>
2020-04-17 11:38:15 +0900

    All the basic settings before Templates

--- Web_board/community/community/settings.py
+++ Web_board/community/community/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1zfer-&5mxd%u1ebkq=4n766h!7^gu7+$hy3ky2m3v@+g(cmh%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'community.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'community','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
---
+++ Web_board/community/reviews/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# 어드민, 사이트, 등록해줘, (모델이름)
+admin.site.register(Review)
