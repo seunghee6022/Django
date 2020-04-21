fdc77b4f851f25aa99bbd508dd1d35d455a089da
Seunghee Jeong <seunghee6022@gmail.com>
2020-04-21 22:59:02 +0900

    Update function | 수정버튼은 get, 제출(form.html)은 post

--- Web_board/community/reviews/views.py
+++ Web_board/community/reviews/views.py
@@ -33,3 +33,17 @@ def detail(request, review_pk):
     return render(request, 'reviews/detail.html', context)
 
         
+def update(request, review_pk):
+    # 호출(수정버튼)은 get, 제출(form.html)은 post
+    review = get_object_or_404(Review, pk=review_pk)
+    if request.method=="POST":
+        form = ReviewForm(request.POST, instance = review )
+        if form.is_valid():
+            review = form.save()
+            return redirect('reviews:detail', review.pk)
+    else :
+        form = ReviewForm(instance=review)
+    context = {
+        'form' : form
+    }
+    return render(request, 'reviews/form.html', context)
